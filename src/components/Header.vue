e06d2797743ec384bfec0ba2c1351b5ccf809dbf
YuanFu <496080373@qq.com>
2017-06-30 16:43:54 +0800

    后台界面优化

---
+++ src/components/Header.vue
@@ -0,0 +1,86 @@
+<template>
+  <navbar>
+    <button class="navbar-toggler mobile-sidebar-toggler d-lg-none" type="button" @click="mobileSidebarToggle">&#9776;</button>
+    <a class="navbar-brand" href="#"></a>
+    <ul class="nav navbar-nav d-md-down-none">
+      <li class="nav-item">
+        <a class="nav-link navbar-toggler sidebar-toggler" href="#" @click="sidebarMinimize">&#9776;</a>
+      </li>
+      <li class="nav-item px-3">
+        <a class="nav-link" href="#">舆情监测</a>
+      </li>
+      <li class="nav-item px-3">
+        <a class="nav-link" href="#">行业指数</a>
+      </li>
+      <li class="nav-item px-3">
+        <a class="nav-link" href="#">研究报告</a>
+      </li>
+      <li class="nav-item px-3">
+        <a class="nav-link" href="#">配置</a>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav ml-auto">
+      <li class="nav-item d-md-down-none">
+        <a class="nav-link" href="#">全屏</a>
+      </li>
+      <li class="nav-item d-md-down-none">
+        <a class="nav-link" href="#">锁屏</a>
+      </li>
+      <li class="nav-item d-md-down-none">
+        <a class="nav-link" href="#">清除缓存</a>
+      </li>
+      <li class="nav-item d-md-down-none">
+        <a class="nav-link" href="#">主题</a>
+      </li>
+      <dropdown size="nav" class="nav-item">
+        <span slot="button">
+          <img src="static/img/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
+          <span class="d-md-down-none">admin</span>
+        </span>
+        <div slot="dropdown-menu"class="dropdown-menu dropdown-menu-right">
+          <a class="dropdown-item" href="#"><i class="fa fa-bell-o"></i> 个人信息</a>
+          <a class="dropdown-item" href="#"><i class="fa fa-envelope-o"></i> 修改密码</a>
+          <a class="dropdown-item" href="#"><i class="fa fa-tasks"></i> 退出</a>
+        </div>
+      </dropdown>
+      <li class="nav-item d-md-down-none">
+        <a class="nav-link navbar-toggler aside-menu-toggler" href="#" @click="asideToggle">&#9776;</a>
+      </li>
+    </ul>
+    <div class="navbar-hrbr"></div>
+  </navbar>
+</template>
+<script>
+
+import navbar from './Navbar'
+import { dropdown } from 'vue-strap'
+
+export default {
+  name: 'header',
+  components: {
+    navbar,
+    dropdown
+  },
+  methods: {
+    click () {
+      // do nothing
+    },
+    sidebarToggle (e) {
+      e.preventDefault()
+      document.body.classList.toggle('sidebar-hidden')
+    },
+    sidebarMinimize (e) {
+      e.preventDefault()
+      document.body.classList.toggle('sidebar-minimized')
+    },
+    mobileSidebarToggle (e) {
+      e.preventDefault()
+      document.body.classList.toggle('sidebar-mobile-show')
+    },
+    asideToggle (e) {
+      e.preventDefault()
+      document.body.classList.toggle('aside-menu-hidden')
+    }
+  }
+}
+</script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="animated fadeIn">
+    <div class="row">
+      <div class="col-sm-6 col-lg-3">
+        <div class="card card-inverse card-primary">
+          <div class="card-block pb-0">
+            <dropdown class="float-right" type="transparent p-0">
+              <i slot="button" class="icon-settings"></i>
+              <div slot="dropdown-menu" class="dropdown-menu dropdown-menu-right">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </dropdown>
+            <h4 class="mb-0">9.823</h4>
+            <p>Members online</p>
+          </div>
+          <card-line1-chart-example class="chart-wrapper px-3" style="height:70px;" height="70"/>
+        </div>
+      </div><!--/.col-->
+
+      <div class="col-sm-6 col-lg-3">
+        <div class="card card-inverse card-info">
+          <div class="card-block pb-0">
+            <dropdown class="float-right" type="transparent p-0">
+              <i slot="button" class="icon-settings"></i>
+              <div slot="dropdown-menu" class="dropdown-menu dropdown-menu-right">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </dropdown>
+            <h4 class="mb-0">9.823</h4>
+            <p>Members online</p>
+          </div>
+          <card-line2-chart-example class="chart-wrapper px-3" style="height:70px;" height="70"/>
+        </div>
+      </div><!--/.col-->
+
+      <div class="col-sm-6 col-lg-3">
+        <div class="card card-inverse card-warning">
+          <div class="card-block pb-0">
+            <div class="btn-group float-right">
+              <dropdown class="float-right" type="transparent p-0">
+                <i slot="button" class="icon-settings"></i>
+                <div slot="dropdown-menu" class="dropdown-menu dropdown-menu-right">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </dropdown>
+            </div>
+            <h4 class="mb-0">9.823</h4>
+            <p>Members online</p>
+          </div>
+          <card-line3-chart-example class="chart-wrapper" style="height:70px;" height="70"/>
+        </div>
+      </div><!--/.col-->
+
+      <div class="col-sm-6 col-lg-3">
+        <div class="card card-inverse card-danger">
+          <div class="card-block pb-0">
+            <div class="btn-group float-right">
+              <dropdown class="float-right" type="transparent p-0">
+                <i slot="button" class="icon-settings"></i>
+                <div slot="dropdown-menu" class="dropdown-menu dropdown-menu-right">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </dropdown>
+            </div>
+            <h4 class="mb-0">9.823</h4>
+            <p>Members online</p>
+          </div>
+          <card-bar-chart-example class="chart-wrapper px-3" style="height:70px;" height="70"/>
+        </div>
+      </div><!--/.col-->
+    </div><!--/.row-->   
+  </div>
+</template>
+
+<script>
+import CardLine1ChartExample from './dashboard/CardLine1ChartExample'
+import CardLine2ChartExample from './dashboard/CardLine2ChartExample'
+import CardLine3ChartExample from './dashboard/CardLine3ChartExample'
+import CardBarChartExample from './dashboard/CardBarChartExample'
+import MainChartExample from './dashboard/MainChartExample'
+import SocialBoxChartExample from './dashboard/SocialBoxChartExample'
+
+import { dropdown } from 'vue-strap'
+
+export default {
+  name: 'dashboard',
+  components: {
+    CardLine1ChartExample,
+    CardLine2ChartExample,
+    CardLine3ChartExample,
+    CardBarChartExample,
+    MainChartExample,
+    SocialBoxChartExample,
+    dropdown
+  }
+}
+</script>
